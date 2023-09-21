728ea20d30497c7ccd1a84d97ec76bd051777c74
Tomy Falgui <tomyfalgui@gmail.com>
2023-09-21 11:18:25 +0800

    Adding a storage stck

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,17 @@
+import { Bucket, StackContext, Table } from 'sst/constructs'
+
+export function StorageStack({ stack }: StackContext) {
+  const bucket = new Bucket(stack, 'Uploads')
+  const table = new Table(stack, 'Notes', {
+    fields: {
+      userId: 'string',
+      noteId: 'string',
+    },
+    primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+  })
+
+  return {
+    bucket,
+    table,
+  }
+}
